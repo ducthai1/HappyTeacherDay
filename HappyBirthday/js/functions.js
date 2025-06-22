c55b1545c97c65b5495b24db7a63c6cf356370d0
Thái Tăng Đức <thaitangduc1@gmail.com>
2025-06-22 23:28:15 +0700

    [TD-1] Remove function typewriter + comment 3 tab link

--- HappyBirthday/js/functions.js
+++ HappyBirthday/js/functions.js
@@ -54,24 +54,3 @@ function timeElapse(date) {
 	var result = "<span class=\"digit\">" + days + "</span> ngày <span class=\"digit\">" + hours + "</span> giờ <span class=\"digit\">" + minutes + "</span> phút <span class=\"digit\">" + seconds + "</span> giây";
 	$("#clock").html(result);
 }
-
-jQuery.fn.typewriter = function () {
-	this.each(function () {
-		var $ele = $(this), str = $ele.html(), progress = 0;
-		$ele.html('');
-		var timer = setInterval(function () {
-			var current = str.substr(progress, 1);
-			if (current == '<') {
-				progress = str.indexOf('>', progress) + 1;
-			} else {
-				progress++;
-			}
-			$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-			if (progress >= str.length) {
-				clearInterval(timer);
-				$ele.html(str); // Hiển thị full nội dung không còn dấu gạch dưới
-			}
-		}, 75);
-	});
-	return this;
-};
